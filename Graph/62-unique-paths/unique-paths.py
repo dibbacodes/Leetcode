177775bcab04409befcd8239b42255ab7b46fadc
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-17 17:24:22 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (10.57%) - LeetSync

--- Graph/62-unique-paths/unique-paths.py
+++ Graph/62-unique-paths/unique-paths.py
@@ -50,6 +50,39 @@ class Solution:
 
         return dp[0][0]
 
+    def spaceOptHelper(self, m, n) -> int:
+        # Initialize required arrays
+        curr = [0]*n
+        next = [0]*n
+        
+        # Base Cases
+        next[n-1] = 1 # One way to reach from end to end
+
+        # Iterate through all cases
+        for row in range(m-1, -1, -1):
+            for col in range(n-1, -1, -1):
+
+                # If last cell: Skip
+                if row == m-1 and col == n-1:
+                    curr[col] = 1
+                    continue
+                
+                # left
+                left = 0
+                if col+1 < n:
+                    left = curr[col+1]
+                
+                # right
+                up = 0
+                if row+1 < m:
+                    up = next[col]
+
+                curr[col] = left + up
+            
+            next = curr
+
+        return next[0]
+
     def uniquePaths(self, m: int, n: int) -> int:
         #### MEMOIZATION ####
 
@@ -60,4 +93,8 @@ class Solution:
         # return self.memoHelper(0, 0, m, n, dp)
 
         ##### TABULATION #####
-        return self.tabulationHelper(m, n)
+        # return self.tabulationHelper(m, n)
+
+        ##### SPACE OPTIMIZATION #####
+        return self.spaceOptHelper(m, n)
+
