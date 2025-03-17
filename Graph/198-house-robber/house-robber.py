74ee72051ea41d098391bb207f3a0ccebb689015
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-17 14:06:45 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (12.92%) - LeetSync

--- Graph/198-house-robber/house-robber.py
+++ Graph/198-house-robber/house-robber.py
@@ -1,23 +1,29 @@
 class Solution:
-    def helper(self, index: int, nums: List[int], 
-    dp: List[int], n: int) -> int:
-        # base case
-        if(index >= n):
+    def helpRob(self, index: int, nums: List[int], dp: List[int], n: int)->int:
+        ## Base Cases
+
+        # If index is greater or equal to n: Not possibe to Rob
+        if index >= n:
             return 0
 
-        # memoization
-        if(dp[index] != -1):
+        ## Check if this value has already been calculated
+        if dp[index] != -1:
             return dp[index]
 
-        # recursion
-        rob = nums[index] + self.helper(index+2, nums, dp, n)
-        not_rob = 0 + self.helper(index+1, nums, dp, n)
+        ## Go through all possible options
+        robHouse = nums[index] + self.helpRob(index+2, nums, dp, n)
+        skipHouse = 0 + self.helpRob(index+1, nums, dp, n)
 
-        dp[index] = max(rob, not_rob)
+        ## Memoization and return maximum value 
+        dp[index] = max(robHouse, skipHouse)
         return dp[index]
 
     def rob(self, nums: List[int]) -> int:
+        # Define size of nums
         n = len(nums)
+
+        # Initialize dp array
         dp = [-1]*n
 
-        return self.helper(0, nums, dp, n)
+        # Call helpRob function and return the answer
+        return self.helpRob(0, nums, dp, n)
