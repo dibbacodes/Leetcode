6f49d15d5411b8507599d4ab32618c9e48b1eecb
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-17 14:25:43 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (12.92%) - LeetSync

--- Graph/198-house-robber/house-robber.py
+++ Graph/198-house-robber/house-robber.py
@@ -19,11 +19,41 @@ class Solution:
         return dp[index]
 
     def rob(self, nums: List[int]) -> int:
+        ##### MEMOIZATION: Top Down #####
+
         # Define size of nums
         n = len(nums)
 
         # Initialize dp array
-        dp = [-1]*n
+        # dp = [-1]*n
 
         # Call helpRob function and return the answer
-        return self.helpRob(0, nums, dp, n)
+        # return self.helpRob(0, nums, dp, n)
+
+        ####### TABULATION: Bottom-Up #######
+
+        ## Inititalize dp array
+        dp = [0]*(n+1)
+
+        ## Base Cases
+
+        # Return 0 if we start robbing after the last house
+        dp[n] = 0
+
+        # Iterate through all the houses
+        for index in range(n-1, -1, -1):
+
+            # Rob the current house
+            robHouse = nums[index]
+            if index+2 < n:
+                robHouse += dp[index+2]
+
+            # Skip the current house
+            skipHouse = 0
+            if index+1 < n:
+                skipHouse += dp[index+1]
+
+            dp[index] = max(robHouse, skipHouse)
+
+        return dp[0]
+            
