4eefe3b37a5ac56b4ac37ba49e87a997e20dd560
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-17 18:58:44 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (71.34%) - LeetSync

--- Graph/63-unique-paths-ii/unique-paths-ii.py
+++ Graph/63-unique-paths-ii/unique-paths-ii.py
@@ -25,20 +25,58 @@ class Solution:
         dp[row][col] = right + down
         return dp[row][col]
 
-
-        
-
-    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
-        # Dimensions of grid
+    def tabHelper(self, grid) -> int:
+        ## Dimensions of grid
         nrows = len(grid)
         ncols = len(grid[0])
 
-        # Return 0: if impossible to reach end
+        ## Return 0: if impossible to reach end
         if grid[nrows-1][ncols-1] == 1 or grid[0][0] == 1:
             return 0
+        
+        ## Initialize dp array
+        dp = [[0]*ncols for _ in range(nrows)]
 
-        # Initialize dp array
-        dp = [[-1]*ncols for _ in range(nrows)]
+        ## Base Cases
+        dp[nrows-1][ncols-1] = 1
 
-        # Call helper funtion and return answer
-        return self.memoHelper(0, 0, grid, nrows, ncols, dp)
+        ## Iterate through all cells in grid
+        for row in range(nrows-1, -1, -1):
+            for col in range(ncols-1, -1, -1):
+
+                if row == nrows-1 and col == ncols-1:
+                    continue
+
+                ## Calculate all possible cases
+                left = 0
+                if col+1 < ncols and not grid[row][col+1] :
+                    left = dp[row][col+1]
+
+                up = 0
+                if row+1 < nrows and not grid[row+1][col]:
+                    up = dp[row+1][col]
+
+                dp[row][col] = left + up
+
+        return dp[0][0]
+        
+
+    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+        ########## MEMOIZATION ##########
+
+        # # Dimensions of grid
+        # nrows = len(grid)
+        # ncols = len(grid[0])
+
+        # # Return 0: if impossible to reach end
+        # if grid[nrows-1][ncols-1] == 1 or grid[0][0] == 1:
+        #     return 0
+
+        # # Initialize dp array
+        # dp = [[-1]*ncols for _ in range(nrows)]
+
+        # # Call helper funtion and return answer
+        # return self.memoHelper(0, 0, grid, nrows, ncols, dp)
+
+        ########## TABULATION ##########
+        return self.tabHelper(grid)
