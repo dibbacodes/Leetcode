02518951f72abb9b4e1b8f7420fe68346e07e1ac
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-17 15:19:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.99%) - LeetSync

--- Graph/213-house-robber-ii/house-robber-ii.py
+++ Graph/213-house-robber-ii/house-robber-ii.py
@@ -18,15 +18,48 @@ class Solution:
         dp[index] = max(robHouse, skipHouse)
         return dp[index]
 
-    def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp1 = [-1]*n
-        dp2 = [-1]*n
+    def tabulationHelpRob(self, nums: List[int], start, end) -> int:
+        # Initialize dp array
+        dp = [0] * (end+1)
 
-        if n==1:
-            return nums[0]
+        # Base Cases: cannot rob house which is not present
+        dp[end] = 0
+
+        # Iterate through all houses
+        for index in range(end-1, start-1, -1):
+            robHouse = nums[index]
+            if index+2 < end:
+                robHouse += dp[index+2]
+
+            skipHouse = 0
+            if index+1 < end:
+                skipHouse += dp[index+1]
+            
+            dp[index] = max(robHouse, skipHouse)
+
+        return dp[start]
+
+
+    def rob(self, nums: List[int]) -> int:
+        # n = len(nums)
+        # dp1 = [-1]*n
+        # dp2 = [-1]*n
+
+        # if n==1:
+        #     return nums[0]
         
-        skipHouse1 = self.helpRob(1, nums, dp1, n)
-        skipHouse2 = self.helpRob(0, nums, dp2, n-1)
+        # skipHouse1 = self.helpRob(1, nums, dp1, n)
+        # skipHouse2 = self.helpRob(0, nums, dp2, n-1)
+
+        # return max(skipHouse1, skipHouse2)
+
+        ######### TABULATION ###########
+        n = len(nums)
+
+        if n == 1:
+            return nums[0]
+
+        skipHouse1 = self.tabulationHelpRob(nums, 1, n)
+        skipHouse2 = self.tabulationHelpRob(nums, 0, n-1)
 
         return max(skipHouse1, skipHouse2)
