c13d516e8a4da2245b65682e84b7d7dd8a9bf20a
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-18 11:24:56 +0530

    Time: 19 ms (37.19%) | Memory: 20.7 MB (21.74%) - LeetSync

--- Graph/64-minimum-path-sum/minimum-path-sum.py
+++ Graph/64-minimum-path-sum/minimum-path-sum.py
@@ -24,15 +24,44 @@ class Solution:
         dp[row][col] = min(right, down)
         return dp[row][col]
 
-    def minPathSum(self, grid: List[List[int]]) -> int:
-        ######## MEMOIZATION ########
-
+    def tabulationHelper(self, grid):
         ## Grid dimension
         nrows = len(grid)
         ncols = len(grid[0])
 
         ## Initialize dp array
-        dp = [[-1]*ncols for _ in range(nrows)]
+        dp = [[self.INF]*ncols for _ in range(nrows)]
 
-        ## Call memoHelper and return result
-        return self.memoHelper(0, 0, grid, nrows, ncols, dp)
+        ## Iterate through all cells of grid
+        for row in range(nrows-1, -1, -1):
+            for col in range(ncols-1, -1, -1):
+                # Base Case: Only one way from last cell to last cell
+                if row == nrows-1 and col == ncols-1:
+                    dp[row][col] = grid[row][col]
+                    continue
+
+                # Calculate all possible options
+                right = grid[row][col] + dp[row][col+1] if col+1 < ncols else self.INF
+                down = grid[row][col] + dp[row+1][col] if row+1 < nrows else self.INF
+
+                # Update dp with min vale
+                dp[row][col] = min(right, down)
+
+        return dp[0][0] 
+
+
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        ######## MEMOIZATION ########
+
+        ## Grid dimension
+        # nrows = len(grid)
+        # ncols = len(grid[0])
+
+        # ## Initialize dp array
+        # dp = [[-1]*ncols for _ in range(nrows)]
+
+        # ## Call memoHelper and return result
+        # return self.memoHelper(0, 0, grid, nrows, ncols, dp)
+
+        ######## TABULATION ########
+        return self.tabulationHelper(grid)
