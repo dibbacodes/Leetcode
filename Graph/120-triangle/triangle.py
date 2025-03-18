3e06d92ba5e873c9d8f2445f1d39f13acb654b30
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-18 13:12:43 +0530

    Time: 3 ms (86.58%) | Memory: 19.7 MB (22.58%) - LeetSync

--- Graph/120-triangle/triangle.py
+++ Graph/120-triangle/triangle.py
@@ -1,4 +1,5 @@
 class Solution:
+    INF = 1e9
     def memoHelper(self, row, col, triangle, dp, nrows):
         ## Base Cases
         if row == nrows-1:
@@ -16,13 +17,41 @@ class Solution:
         dp[row][col] = min(down, up)
         return dp[row][col]
 
-    def minimumTotal(self, triangle: List[List[int]]) -> int:
-        ########## MEMOIZATION #############
+    def tabulationHelper(self, triangle):
         ## Triangle Dimensions
         nrows = len(triangle)
 
         ## Initialize dp array
-        dp = [[-1]*nrows for _ in range(nrows)]
+        dp = [[self.INF]*nrows for _ in range(nrows)]
 
-        ## Call memoHelper and return answer
-        return self.memoHelper(0, 0, triangle, dp, nrows)
+        ## Iterate through all possible options
+        for row in range(nrows-1, -1, -1):
+            for col in range(row, -1, -1):
+                # Base Case: Last Row
+                if row == nrows-1:
+                    dp[row][col] = triangle[row][col]
+                    continue
+
+                # Calculate all possible cases
+                down = triangle[row][col] + dp[row+1][col]
+                up = triangle[row][col] + dp[row+1][col+1]
+        
+                # Update dp cell with min value
+                dp[row][col] = min(down, up)
+
+        return dp[row][col]
+
+
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        # ########## MEMOIZATION #############
+        # ## Triangle Dimensions
+        # nrows = len(triangle)
+
+        # ## Initialize dp array
+        # dp = [[-1]*nrows for _ in range(nrows)]
+
+        # ## Call memoHelper and return answer
+        # return self.memoHelper(0, 0, triangle, dp, nrows)
+
+        ########## TABULATION ##########
+        return self.tabulationHelper(triangle)
