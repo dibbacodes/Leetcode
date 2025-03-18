b33a627ffe4e9b67559956fbe482ef5625ed93fc
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-18 19:56:30 +0530

    Time: 25 ms (48.19%) | Memory: 19 MB (34.91%) - LeetSync

--- Graph/967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ Graph/967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -23,7 +23,7 @@ class Solution:
         dp[row][col] = min(left, right, down)
         return dp[row][col]
 
-    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+    def tabulationHelper(self, matrix):
         ## Matrix Dimensions
         nrows = len(matrix)
         ncols = len(matrix[0])
@@ -31,9 +31,43 @@ class Solution:
         ## Initialize dp array
         dp = [[self.INF]*ncols for _ in range(nrows)]
 
-        ## Start faling from every element of first row
+        ## Iterate through all options
         answer = self.INF
-        for col in range(ncols):
-            answer = min(answer, self.memoHelper(0, col, matrix, nrows, ncols, dp))
+        for row in range(nrows-1, -1, -1):
+            for col in range(ncols-1, -1, -1):
+                ## Base Case: Last Row
+                if row == nrows-1:
+                    dp[row][col] = matrix[row][col]
+                    continue
 
-        return answer
+                ## Calculate all possible cases
+                left = matrix[row][col] + dp[row+1][col-1] if col-1 >= 0 else self.INF
+                right = matrix[row][col] + dp[row+1][col+1] if col+1 < ncols else self.INF
+                down = matrix[row][col] + dp[row+1][col]
+
+                ## Update cell value in dp table
+                dp[row][col] = min(left, right, down)
+
+        return min(dp[0])
+
+        
+
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        ############### MEMOIZATION ###############
+
+        # ## Matrix Dimensions
+        # nrows = len(matrix)
+        # ncols = len(matrix[0])
+
+        # ## Initialize dp array
+        # dp = [[self.INF]*ncols for _ in range(nrows)]
+
+        # ## Start faling from every element of first row
+        # answer = self.INF
+        # for col in range(ncols):
+        #     answer = min(answer, self.memoHelper(0, col, matrix, nrows, ncols, dp))
+
+        # return answer
+
+        ############ TABULATION ############
+        return self.tabulationHelper(matrix)
