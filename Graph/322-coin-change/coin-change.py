1d82ca69a10df6ba301791b05c654c57982a18be
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-20 22:11:35 +0530

    Time: 967 ms (28.37%) | Memory: 21 MB (24.05%) - LeetSync

--- Graph/322-coin-change/coin-change.py
+++ Graph/322-coin-change/coin-change.py
@@ -21,17 +21,48 @@ class Solution:
         dp[index][target] = min(take, not_take)
         return dp[index][target]
 
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        ######### MEMOIZATION ########
-        
+    def tabulationHelper(self, coins, amount):
         ## Number of coins 
         n = len(coins)
 
         ## Initialize the dp array
-        dp = [[-1]*(amount+1) for _ in range(n)]
+        dp = [[self.INF]*(amount+1) for _ in range(n)]
 
-        ## Call helper function and return answer
-        result = self.memoHelper(0, amount, coins, dp, n)
-        return result if result != 1e9 else -1
+        ## Base Cases
+        # Fill the last row
+        for target in range(amount+1):
+            if target % coins[n-1] == 0:
+                dp[n-1][target] = target//coins[n-1]
+
+        ## Iterate over the array
+        for index in range(n-2, -1, -1):
+            for target in range(0, amount+1, 1):
+                ## Calculate all possible options
+                take = 1 + dp[index][target-coins[index]] if target >= coins[index] else self.INF
+                not_take = 0 + dp[index+1][target] 
+                
+                ## Update dp table
+                dp[index][target] = min(take, not_take)
+
+        if dp[0][amount] >= self.INF:
+            return -1
+        else:
+            return dp[0][amount]
+
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        ######### MEMOIZATION ########
+        
+        # ## Number of coins 
+        # n = len(coins)
+
+        # ## Initialize the dp array
+        # dp = [[-1]*(amount+1) for _ in range(n)]
+
+        # ## Call helper function and return answer
+        # result = self.memoHelper(0, amount, coins, dp, n)
+        # return result if result != 1e9 else -1
+
+        ###### TABULTION ######
+        return self.tabulationHelper(coins, amount)
 
         
