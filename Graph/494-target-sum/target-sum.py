37458ebec7a81132307393901a1e1dd03c79db91
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-21 12:45:24 +0530

    Time: 44 ms (87.21%) | Memory: 17.9 MB (90.70%) - LeetSync

--- Graph/494-target-sum/target-sum.py
+++ Graph/494-target-sum/target-sum.py
@@ -19,14 +19,7 @@ class Solution:
         dp[index][target] = take + notTake
         return dp[index][target]
 
-    def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        ## Logic
-        # total = sum of all elements of array
-        # We essentially need S1 - S2 = target
-        # S1 + S2 = total
-        # S1 = (total + target)//2
-
-        ####### MEMOIZATION ######
+    def tabulationHelper(self, nums, target):
         ## Array size
         n = len(nums)
 
@@ -40,7 +33,50 @@ class Solution:
             newTarget = (target+total)//2
 
         ## Initialize dp array
-        dp = [[-1]*(newTarget+1) for _ in range(n)]
+        dp = [[0]*(newTarget+1) for _ in range(n+1)]
 
-        ## Call function and return answer
-        return self.memoHelper(0, newTarget, nums, dp, n)
+        ## Base Cases
+        dp[n][0] = 1
+        # dp[n][target] = 0 for all target in range
+
+        ## Iterate over the array
+        for index in range(n-1, -1, -1):
+            for amount in range(0, newTarget+1, 1):
+                ## Calculate possible cases
+                take = dp[index+1][amount-nums[index]] if amount >= nums[index] else 0
+                notTake = dp[index+1][amount]
+
+                ## Update dp table
+                dp[index][amount] = take + notTake
+
+        return dp[0][newTarget]
+
+
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        ## Logic
+        # total = sum of all elements of array
+        # We essentially need S1 - S2 = target
+        # S1 + S2 = total
+        # S1 = (total + target)//2
+
+        ####### MEMOIZATION ######
+        ## Array size
+        # n = len(nums)
+
+        # ## Sum of all elements
+        # total = sum(nums)
+
+        # ## New target
+        # if (target + total) % 2 != 0 or total < abs(target):
+        #     return 0
+        # else:
+        #     newTarget = (target+total)//2
+
+        # ## Initialize dp array
+        # dp = [[-1]*(newTarget+1) for _ in range(n)]
+
+        # ## Call function and return answer
+        # return self.memoHelper(0, newTarget, nums, dp, n)
+
+        ###### TABULATION ######
+        return self.tabulationHelper(nums, target)
