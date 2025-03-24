b41d24ac5b6dc9417434805abda436d12f4d3194
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-24 23:32:08 +0530

    Time: 417 ms (83.16%) | Memory: 42.8 MB (59.17%) - LeetSync

--- Graph/1250-longest-common-subsequence/longest-common-subsequence.py
+++ Graph/1250-longest-common-subsequence/longest-common-subsequence.py
@@ -24,13 +24,42 @@ class Solution:
 
 
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        #### MEMOIZATION ####
+        # #### MEMOIZATION ####
+        # ## Size of strings
+        # l1 = len(text1)
+        # l2 = len(text2)
+
+        # ## Initialize dp array
+        # dp = [[-1]*l2 for _ in range(l1)]
+
+        # ## Call memoHelper and return answer
+        # return self.memoHelper(0, 0, l1, l2, dp, text1, text2)
+
+        ##### TABULATION #####
+        
         ## Size of strings
         l1 = len(text1)
         l2 = len(text2)
 
         ## Initialize dp array
-        dp = [[-1]*l2 for _ in range(l1)]
+        dp = [[0]*(l2+1) for _ in range(l1+1)]
 
-        ## Call memoHelper and return answer
-        return self.memoHelper(0, 0, l1, l2, dp, text1, text2)
+        ## Base cases
+        # dp[index1][l2] = 0
+        # dp[l1][index2] = 0
+
+        ## Iterate over the strings
+        for index1 in range(l1-1, -1, -1):
+            for index2 in range(l2-1, -1, -1):
+                # Letters match
+                if text1[index1] == text2[index2]:
+                    dp[index1][index2] = 1 + dp[index1+1][index2+1]
+
+                # Letters don't match
+                else:
+                    moveIndex1 = dp[index1+1][index2]
+                    moveIndex2 = dp[index1][index2+1]
+
+                    dp[index1][index2] = max(moveIndex1, moveIndex2)
+
+        return dp[0][0]
