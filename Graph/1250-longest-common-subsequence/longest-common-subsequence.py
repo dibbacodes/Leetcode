b29a9af3aaa7ef7b37b5876362d6c800388838fb
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-24 23:44:06 +0530

    Time: 344 ms (94.88%) | Memory: 18.2 MB (88.03%) - LeetSync

--- Graph/1250-longest-common-subsequence/longest-common-subsequence.py
+++ Graph/1250-longest-common-subsequence/longest-common-subsequence.py
@@ -22,21 +22,7 @@ class Solution:
             dp[index1][index2] = max(moveIndex1, moveIndex2)
             return dp[index1][index2]
 
-
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # #### MEMOIZATION ####
-        # ## Size of strings
-        # l1 = len(text1)
-        # l2 = len(text2)
-
-        # ## Initialize dp array
-        # dp = [[-1]*l2 for _ in range(l1)]
-
-        # ## Call memoHelper and return answer
-        # return self.memoHelper(0, 0, l1, l2, dp, text1, text2)
-
-        ##### TABULATION #####
-        
+    def tabulationHelper(self, text1, text2):
         ## Size of strings
         l1 = len(text1)
         l2 = len(text2)
@@ -63,3 +49,51 @@ class Solution:
                     dp[index1][index2] = max(moveIndex1, moveIndex2)
 
         return dp[0][0]
+ 
+    def spaceOptHelper(self, text1, text2):
+        ## Size of strings
+        l1 = len(text1)
+        l2 = len(text2)
+
+        ## Initialize base case array: ahead
+        ahead = [0]*(l2+1) 
+
+        ## Base cases
+        # ahead[l2] = 0
+
+        ## Iterate over the strings
+        for index1 in range(l1-1, -1, -1):
+            curr = [0]*(l2+1)
+            for index2 in range(l2-1, -1, -1):
+                # Letters match
+                if text1[index1] == text2[index2]:
+                    curr[index2] = 1 + ahead[index2+1]
+
+                # Letters don't match
+                else:
+                    moveIndex1 = ahead[index2]
+                    moveIndex2 = curr[index2+1]
+
+                    curr[index2] = max(moveIndex1, moveIndex2)
+
+            ahead = curr.copy()
+
+        return ahead[0]
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # #### MEMOIZATION ####
+        # ## Size of strings
+        # l1 = len(text1)
+        # l2 = len(text2)
+
+        # ## Initialize dp array
+        # dp = [[-1]*l2 for _ in range(l1)]
+
+        # ## Call memoHelper and return answer
+        # return self.memoHelper(0, 0, l1, l2, dp, text1, text2)
+
+        ##### TABULATION #####
+        # return self.tabulationHelper(text1, text2)
+
+        ##### SPACE OPTIMIZATION #####
+        return self.spaceOptHelper(text1, text2)
