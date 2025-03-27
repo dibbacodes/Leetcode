48b9a4f71c9d16bfda00ab1c64bc07e26a299107
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-28 00:56:13 +0530

    Time: 59 ms (71.08%) | Memory: 17.8 MB (95.96%) - LeetSync

--- Graph/72-edit-distance/edit-distance.py
+++ Graph/72-edit-distance/edit-distance.py
@@ -30,13 +30,50 @@ class Solution:
             dp[index1][index2] = min(replace, insert, delete)
             return dp[index1][index2]
 
-    def minDistance(self, word1: str, word2: str) -> int:
+    def spaceOptHelper(self, word1, word2):
         ## Length of words
         l1 = len(word1)
         l2 = len(word2)
 
-        ## Initialize dp array
-        dp = [[-1]*(l2+1) for _ in range(l1+1)]
+        ## Initialize base case array
+        ahead = [0]*(l2+1)
 
-        ## Call memoHelper and return answer
-        return self.memoHelper(0, 0, word1, word2, dp)
+        ## Base case
+        for index2 in range(l2+1):
+            ahead[index2] = l2 - index2
+
+        ## Iterate over the strings 
+        for index1 in range(l1-1, -1, -1):
+            curr = [0]*(l2+1)
+            curr[l2] = l1 - index1
+            for index2 in range(l2-1, -1, -1):
+                ## Calculate all cases
+                # Letters match
+                if word1[index1] == word2[index2]:
+                    curr[index2] = ahead[index2+1]
+
+                # Letters don't match
+                else:
+                    replace = 1 + ahead[index2+1]
+                    insert = 1 + curr[index2+1]
+                    delete = 1 + ahead[index2]
+
+                    curr[index2] = min(replace, insert, delete)
+
+            ahead = curr.copy()
+
+        return ahead[0]              
+
+    def minDistance(self, word1: str, word2: str) -> int:
+        # ## Length of words
+        # l1 = len(word1)
+        # l2 = len(word2)
+
+        # ## Initialize dp array
+        # dp = [[-1]*(l2+1) for _ in range(l1+1)]
+
+        # ## Call memoHelper and return answer
+        # return self.memoHelper(0, 0, word1, word2, dp)
+
+        ##### SPACE OPTIMIZATION ####
+        return self.spaceOptHelper(word1, word2)
