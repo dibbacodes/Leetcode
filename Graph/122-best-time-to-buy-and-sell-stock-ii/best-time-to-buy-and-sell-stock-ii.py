17e8aceec09fbd6a98ca75f2243a36722c871ab6
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-31 14:24:02 +0530

    Time: 19 ms (5.04%) | Memory: 27.6 MB (5.29%) - LeetSync

--- Graph/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ Graph/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -10,7 +10,7 @@ class Solution:
 
         ## Calculate all possible cases
         if canBuy:
-            buy = -prices[index] + self.memoHelper(index, 0, prices, dp, n)
+            buy = -prices[index] + self.memoHelper(index+1, 0, prices, dp, n)
             dontBuy = self.memoHelper(index+1, 1, prices, dp, n)
 
             dp[index][canBuy] = max(buy, dontBuy)
@@ -30,7 +30,7 @@ class Solution:
         n = len(prices)
 
         ## Initialize dp array
-        dp = [[-1]*2]*(n+1)
+        dp = [[-1]*2 for _ in range(n+1)]
 
         ## Call memoHelper and return answer
         return self.memoHelper(0, 1, prices, dp, n)
