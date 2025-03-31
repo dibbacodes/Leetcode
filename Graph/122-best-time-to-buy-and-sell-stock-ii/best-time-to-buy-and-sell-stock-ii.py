0c49221cbd4667460fb04bae7e51f716a9f66087
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-03-31 15:04:50 +0530

    Time: 13 ms (6.09%) | Memory: 21.3 MB (5.29%) - LeetSync

--- Graph/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ Graph/122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -23,14 +23,45 @@ class Solution:
             dp[index][canBuy] = max(sell, dontSell)
             return dp[index][canBuy]
 
-    def maxProfit(self, prices: List[int]) -> int:
-        #### MEMOIZATION ####
-
+    def tabulationHelper(self, prices):
         ## Length of array
         n = len(prices)
 
         ## Initialize dp array
-        dp = [[-1]*2 for _ in range(n+1)]
+        dp = [[0]*2 for _ in range(n+1)]
 
-        ## Call memoHelper and return answer
-        return self.memoHelper(0, 1, prices, dp, n)
+        ## Base Case
+        # dp[n][canBuy] = 0
+
+        ## Iterate over the array and update dp table
+        for index in range(n-1, -1, -1):
+            for canBuy in range(1, -1, -1):
+                ## Calculate all possible cases
+                if canBuy:
+                    buy = -prices[index] + dp[index+1][0]
+                    dontBuy = dp[index+1][1]
+
+                    dp[index][canBuy] = max(buy, dontBuy)
+
+                else:
+                    sell = prices[index] + dp[index+1][1]
+                    dontSell = dp[index+1][0]
+
+                    dp[index][canBuy] = max(sell, dontSell)
+
+        return dp[0][1]
+
+    def maxProfit(self, prices: List[int]) -> int:
+        # #### MEMOIZATION ####
+
+        # ## Length of array
+        # n = len(prices)
+
+        # ## Initialize dp array
+        # dp = [[-1]*2 for _ in range(n+1)]
+
+        # ## Call memoHelper and return answer
+        # return self.memoHelper(0, 1, prices, dp, n)
+
+        ### TABULATION ####
+        return self.tabulationHelper(prices)
