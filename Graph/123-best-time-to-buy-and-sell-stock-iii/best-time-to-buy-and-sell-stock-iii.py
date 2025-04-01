e16bc92b59577bd98165a13a2b9854b29ded0c91
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-04-01 12:42:35 +0530

    Time: 869 ms (24.86%) | Memory: 29 MB (53.41%) - LeetSync

--- Graph/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ Graph/123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -55,6 +55,40 @@ class Solution:
                         dp[index][canBuy][limit] = max(sell, dontSell)
             
         return dp[0][1][2]
+
+    def spaceOptHelper(self, prices):
+        ## Length of array
+        n = len(prices)
+
+        ## Base case array
+        ahead = [[0]*3 for _ in range(2)]
+
+        ## Base cases
+        # ahead[canBuy][limit] = 0
+        # ahead[canBuy][0] = 0
+
+        ## Iterate over the prices array
+        for index in range(n-1, -1, -1):
+            # Curr array initialization
+            curr = [[0]*3 for _ in range(2)]
+            for canBuy in range(1, -1, -1):
+                for limit in range(2, 0, -1):
+                    ## Calculate all possible cases
+                    if canBuy:
+                        buy = -prices[index] + ahead[0][limit]
+                        dontBuy = ahead[canBuy][limit]
+
+                        curr[canBuy][limit] = max(buy, dontBuy)
+
+                    else:
+                        sell = prices[index] + ahead[1][limit-1]
+                        dontSell = ahead[0][limit]
+
+                        curr[canBuy][limit] = max(sell, dontSell)
+
+            ahead = curr.copy()
+            
+        return ahead[1][2]
         
     def maxProfit(self, prices: List[int]) -> int:
         ####### MEMOIZATION #########
@@ -68,5 +102,10 @@ class Solution:
         # ## Call memoHelper and return answer
         # return self.memoHelper(0, 1, 2, prices, dp, n)
 
-        ###### SPACE OPTIMIZATION ######
-        return self.tabulationHelper(prices)
+        ###### TABULATION ######
+        # return self.tabulationHelper(prices)
+
+        ###### SPACE OPTIMIZATION #######
+        return self.spaceOptHelper(prices)
+
+    
