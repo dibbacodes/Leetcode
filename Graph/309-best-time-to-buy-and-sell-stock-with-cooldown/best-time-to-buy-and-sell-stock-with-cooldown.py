e7b351efd4d38fb0a946883c6283a04f80b7d418
Divyansh Nigam <164505311+dibbacodes@users.noreply.github.com>
2025-04-01 15:01:20 +0530

    Time: 3 ms (74.43%) | Memory: 18.3 MB (68.88%) - LeetSync

--- Graph/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ Graph/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -23,14 +23,47 @@ class Solution:
             dp[index][canBuy] = max(sell, dontSell)
             return dp[index][canBuy]
 
-    def maxProfit(self, prices: List[int]) -> int:
-        ####### MEMOIZATION ########
-
-        ## Length of prices
+    def spaceOptHelper(self, prices):
+        ## Length of array
         n = len(prices)
 
-        ## Initialize dp array
-        dp = [[-1]*(2) for _ in range(n+1)]
+        ## Initialize base case arrays
+        ahead = [0]*(2)
+        ahead2 = [0]*(2)
 
-        ## Call memoHelper and return answer
-        return self.memoHelper(0, 1, prices, dp, n)
+        ## Iterate over the array
+        for index in range(n-1, -1, -1):
+            curr = [0]*(2)
+            for canBuy in range(1, -1, -1):
+                ## Calculate all possible cases
+                if canBuy:
+                    buy = -prices[index] + ahead[0]
+                    dontBuy = ahead[1]
+
+                    curr[canBuy] = max(buy, dontBuy)
+                else:
+                    sell = prices[index] + ahead2[1]
+                    dontSell = ahead[0]
+
+                    curr[canBuy] = max(sell, dontSell)
+
+            ahead2 = ahead.copy()
+            ahead = curr.copy()
+
+        return ahead[1]
+
+    def maxProfit(self, prices: List[int]) -> int:
+        # ####### MEMOIZATION ########
+
+        # ## Length of prices
+        # n = len(prices)
+
+        # ## Initialize dp array
+        # dp = [[-1]*(2) for _ in range(n+1)]
+
+        # ## Call memoHelper and return answer
+        # return self.memoHelper(0, 1, prices, dp, n)
+
+
+        ###### SPACE OPTIMIZATION ######
+        return self.spaceOptHelper(prices)
